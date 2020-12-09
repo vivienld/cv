7288a5701a5ea3ea5bf8a2aaf8c314087cbc1e5d
Vivien Le Duc <leducvivien@gmail.com>
2020-12-09 09:44:26 +0100

    maj

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { artemis } from 'artemis';
 
 const container = artemis('col row vertical')
-const col = artemis("cursor-pointer light-hover col margin-y-1")
+const col = artemis("cursor-pointer light-hover col padding-y-2")
 const contact = artemis('decoration-none text-inherit flex-1 flex-col align-items-center height-100')
 
 export default function Contact({ data }) {
@@ -14,7 +14,7 @@ export default function Contact({ data }) {
                     <div className={col}>
                         <a href={c.link} className={contact} >
                             <div className={[c.icon, artemis('')].join(' ')}></div>
-                            <div className={artemis('flex-row align-items-center height-100')} key={i}>{c.text}</div>
+                            <div className={artemis('flex-row align-items-flex-end height-100')} key={i}>{c.text}</div>
                         </a>
                     </div>
                 ))
